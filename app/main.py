c684e56e12170923a8a90dbf99488d69f67227a4
zbyju <zbynek.jurica@gmail.com>
2024-09-23 20:52:47 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,22 +1,24 @@
 import socket
+import threading
 from .libs import kafka_protocol as kafka
 
 
 def handle_client(client: socket.socket):
-	request = client.recv(2048)
+	while True:
+		request = client.recv(2048)
 
-	header = kafka.HeaderV2.decode(request)
-	body = kafka.Body("")
-	message = kafka.Message(header, body)
-	client.sendall(message.encode())
+		header = kafka.HeaderV2.decode(request)
+		body = kafka.Body("")
+		message = kafka.Message(header, body)
+		client.sendall(message.encode())
 
 
 def main():
 	server = socket.create_server(("localhost", 9092), reuse_port=True)
-	client, addr = server.accept()
 
 	while True:
-		handle_client(client)
+		client, addr = server.accept()
+		threading.Thread(target=handle_client, args=(client,)).start()
 
 
 if __name__ == "__main__":
