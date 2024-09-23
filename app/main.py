162b7134568e3be1b4d54a5668817899ce2ffe45
zbyju <zbynek.jurica@gmail.com>
2024-09-23 20:47:34 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,20 +3,19 @@ from .libs import kafka_protocol as kafka
 
 
 def handle_client(client: socket.socket):
-	request = client.recv(1024)
+	request = client.recv(2048)
 
 	header = kafka.HeaderV2.decode(request)
 	body = kafka.Body("")
 	message = kafka.Message(header, body)
 	client.sendall(message.encode())
-	client.close()
 
 
 def main():
 	server = socket.create_server(("localhost", 9092), reuse_port=True)
+	client, addr = server.accept()
 
 	while True:
-		client, addr = server.accept()
 		handle_client(client)
 
 
